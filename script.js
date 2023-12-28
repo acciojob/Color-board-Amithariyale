3987dc584d74a9750eedb4286d312289ee9b99c8
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-28 19:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const container=document.querySelector(".container");
+
+for(let i=0;i<800;i++){
+	const square=document.createElement('div');
+	square.className='square';
+	container.appendChild(square);
+}
